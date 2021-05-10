6b26d021d0ac186181c573bb645e61ac42a456fb
jonasbucherETH <71451797+jonasbucherETH@users.noreply.github.com>
2021-05-10 16:04:24 +0200

    Add files via upload

---
+++ data_exploration.R
@@ -0,0 +1,164 @@
+# data exploration sports networks: shots
+
+library(dplyr)
+library(StatsBombR)
+library(ggplot2)
+
+StatsBombData <- StatsBombFreeEvents()
+
+
+Comp <- FreeCompetitions()
+
+# all matches in all comps
+Matches <- FreeMatches(Comp)
+names(Matches)
+
+# filter only barca matches
+BarcaMatches <- Matches[Matches$away_team.away_team_name=="Barcelona" | Matches$home_team.home_team_name=="Barcelona",]
+
+# barca lineups
+lineups <- StatsBombFreeLineups(MatchesDF = BarcaMatches)
+lineups <- lineups[lineups$team_name=="Barcelona",]
+
+'''
+# games per player: to do
+games_PerPlayer <- lineups %>%
+'''
+
+#Matches[Matches$away_team.away_team_name=="Barcelona" | Matches$home_team.home_team_name=="Barcelona",]
+
+BarcaEvents <- StatsBombData[StatsBombData$team.name=="Barcelona",]
+names(BarcaEvents)
+
+
+
+##### number of xG
+# xG = shot.statsbomb_xg
+shots <- BarcaEvents[BarcaEvents$type.name=="Shot",]
+
+ShotsPerGame <- 7742/448
+ShotsPerGame # 17.28125
+
+stats_PerPlayer <- shots %>%
+  group_by(player.name) %>%
+  summarise(total_shots = n(), total_goals = sum(shot.outcome.name=="Goal"), xG = sum(shot.statsbomb_xg))
+
+'''
+stats_PerPlayer_Per90 <- shots %>%
+  group_by(player.name, match_id) %>%
+  summarise(total_shots = n(), total_goals = sum(shot.outcome.name=="Goal"), xG = sum(shot.statsbomb_xg))
+'''
+
+
+# top 15 in total goals
+top_15 <- top_n(stats_PerPlayer, 15, total_goals)
+
+ggplot(top_15, aes(y=total_goals, x=xG, color=player.name)) +
+  geom_point(size=3) +
+  labs(title="Top 15 - Goals vs. xG") +
+  ylab("Goals") +
+  xlab("Expected Goals") +
+  geom_abline(slope=1, intercept=0)
+  
+p <- ggplot(top_15, aes(x=player.name, y=total_shots)) +
+  geom_bar(stat="identity") +
+  labs(title="Top 15 Shot Takers") +
+  ylab("Shots taken") +
+  xlab("Player")
+p + theme(axis.text.x = element_text(angle = 90, size=8))
+
+p2 <- ggplot(goals_PerPlayer, aes(x=player.name, y=total_shots)) +
+  geom_bar(stat="identity") +
+  ylab("Total Goals")
+p2 + theme(axis.text.x = element_text(angle = 90))
+
+#combined <- cbind(goals_PerPlayer$total_shots, total_xG$xG)
+#ggplot(data=cbind(goals_PerPlayer, total_xG))
+
+# number of goals
+# number of xA
+# number of assists
+
+
+### table with lineup, goals, match, season, minutes played together
+# note: take minute to make it easy
+# substitution.replacement.name
+
+elapsed_events <- formatelapsedtime(BarcaEvents)
+player_minutes <- get.minutesplayed(elapsed_events)
+
+# also sort by alphabet the list?
+"""
+lineups_games <- player_minutes %>%
+  group_by(match_id) %>%
+  summarise(lineup_1=list(player.id[1:11]), time_1=min(TimeOff),
+            lineup_2=list(player.id[1:11]!=player.id[order(TimeOff[2])]),
+            time_2=TimeOff[order(TimeOff[2])]-min(TimeOff))
+"""
+lineups_games <- player_minutes %>%
+  group_by(match_id) %>%
+  summarise(lineup_1=list(player.id[1:11]), time_1=min(TimeOff),
+            sub_1=min(TimeOff), lineup_2=list(player.id[1:12][-order(TimeOff)[1]]),
+            time_2=TimeOff[order(TimeOff)[2]]-min(TimeOff),
+            sub_2=TimeOff[order(TimeOff)[2]],
+            lineup_3=list(player.id[1:13][-order(TimeOff)[1:2]]),
+            time_3=TimeOff[order(TimeOff)[3]]-TimeOff[order(TimeOff)[2]],
+            sub_3=TimeOff[order(TimeOff)[3]],
+            lineup_4=list(player.id[1:14][-order(TimeOff)[1:3]]),
+            time_4=TimeOff[order(TimeOff)[4]]-TimeOff[order(TimeOff)[3]])
+
+# test
+lineups_games$lineup_1[[1]]
+lineups_games$lineup_2[[1]]
+lineups_games$lineup_3[[1]]
+lineups_games$lineup_4[[1]]
+
+lineups_games[,"goals_1"] <- 0
+lineups_games[,"goals_2"] <- 0
+lineups_games[,"goals_3"] <- 0
+lineups_games[,"goals_4"] <- 0
+
+
+# add goals
+all_goals <- shots[shots$shot.outcome.name=="Goal",]
+
+goals_by_match <- all_goals %>%
+  group_by(match_id, minute) %>%
+  summarise(goals=n())
+
+for (i in 1:nrow(goals_by_match)){
+  id = goals_by_match[[i, 1]]
+  min = goals_by_match[[i, 2]]
+  
+  #current_lineup = lineups_games[lineups_games$match_id==id,]
+  t1 = lineups_games[lineups_games$match_id==id,]$sub_1
+  t2 = lineups_games[lineups_games$match_id==id,]$sub_2
+  t3 = lineups_games[lineups_games$match_id==id,]$sub_3
+  
+  if (min<t1){
+    lineups_games[lineups_games$match_id==id,]$goals_1 = lineups_games[lineups_games$match_id==id,]$goals_1 + 1
+  }
+  else if (t1<min & min<t2){
+    lineups_games[lineups_games$match_id==id,]$goals_2 = lineups_games[lineups_games$match_id==id,]$goals_2 + 1
+  }
+  else if (t2<min & min<t3){
+    lineups_games[lineups_games$match_id==id,]$goals_3 = lineups_games[lineups_games$match_id==id,]$goals_3 + 1
+  }
+  else{
+    lineups_games[lineups_games$match_id==id,]$goals_4 = lineups_games[lineups_games$match_id==id,]$goals_4 + 1
+  }
+
+}
+
+library(data.table)
+long_lineups_games <- melt(lineups_games,  id.vars="match_id", measure.vars=c("lineup_1", "time_1", "goals_1"))
+
+unlist(lineups_games[1,2])
+lineups_unlisted <- cbind(lineups_games[!sapply(lineups_games, is.list)], 
+                          (t(apply(lineups_games[sapply(lineups_games, is.list)], 1, unlist))))
+
+write.csv(lineups_unlisted, "lineups_unlisted", row.names = FALSE)
+
+
+
+
